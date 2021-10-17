6dc39dc37a2a5c0517eebbd1d2f2d330c9084dd8
Licymnius <licymnius@outlook.com>
2021-10-17 07:15:13 +0300

    Update cmd.cpp

--- SVN/Server/game/src/cmd.cpp
+++ SVN/Server/game/src/cmd.cpp
@@ -10,10 +10,10 @@ ACMD(do_pm_all_send);
 
 //find again;
 
-	{ "do_clear_affect",		do_clear_affect,		0,			POS_DEAD,	GM_LOW_WIZARD},
+			{ "do_clear_affect",		do_clear_affect,		0,			POS_DEAD,	GM_LOW_WIZARD},
 
 //add below;
 
 #ifdef ENABLE_PM_ALL_SEND_SYSTEM
-	{ "pm_all_send",			do_pm_all_send,			0,			POS_DEAD,	GM_IMPLEMENTOR},
+			{ "pm_all_send",			do_pm_all_send,			0,			POS_DEAD,	GM_IMPLEMENTOR},
 #endif
